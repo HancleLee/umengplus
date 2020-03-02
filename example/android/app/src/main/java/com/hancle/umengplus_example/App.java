a8ed2fcca4de71b640d88d2b4209491df6b0764c
Hancle <hancle@HancledeMac-mini-2.local>
2020-03-02 11:28:08 +0800

    example

--- example/android/app/src/main/java/com/hancle/umengplus_example/App.java
+++ example/android/app/src/main/java/com/hancle/umengplus_example/App.java
@@ -8,6 +8,6 @@ public class App extends FlutterApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        UmengplusPlugin.init(this, "5e4c90b04ca357e03c00019e", "ceshi", "e214bf89a3549170899c8d0c8b6b0522");
+        UmengplusPlugin.init(this, "xxx", "umeng", "xxxx");
     }
 }
