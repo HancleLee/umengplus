8f0d3e90604208337d04c411436bd494692ebe2c
Hancle <hancle@HancledeMac-mini-2.local>
2020-03-02 19:51:45 +0800

    fixed bug

--- lib/umengplus.dart
+++ lib/umengplus.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 class Umengplus {
   static const MethodChannel _channel =
-      const MethodChannel('ygmpkk/flutter_umplus');
+      const MethodChannel('hancle/umengplus');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
